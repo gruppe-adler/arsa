d6b04d0aeb213edbb60f1edad73506602d6babf7
y0014984 <y0014984@gmail.com>
2025-01-28 15:54:56 +0100

    updated configs

---
+++ .history/auth-proxy-config-arsa-backend/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    authErrorMessage: 'Du bist kein Adler, oder nicht auf forum.gruppe-adler.de eingeloggt',
+    userUrl: 'https://forum.gruppe-adler.de/api/self',
+    cookie: 'express.sid',
+    group: 'Adler',
+    port: 80,
+    target: 'http://arsa-backend:80',
+    whitelist: []
+  }
